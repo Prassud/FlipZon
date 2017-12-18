e724554a0868cf796be7aafd1a0cae524dd3f1b4
siva <siva@insiva.local>
2017-12-18 12:42:20 +0530

    Intial sprint boot set up

---
+++ src/main/java/com/flipzon/flipzon/FlipzonApplication.java
@@ -0,0 +1,12 @@
+package com.flipzon.flipzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipzonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flipzon/flipzon/FlipzonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flipzon.flipzon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlipzonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
